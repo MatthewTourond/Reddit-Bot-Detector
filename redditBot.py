edd9c9a0514e9a43d7fd9997b224c8c0cca79d7a
MatthewTourond <41418306+mtfilo@users.noreply.github.com>
2019-02-18 11:06:00 -0800

    Updated bot replies

--- redditBot.py
+++ redditBot.py
@@ -1,60 +1,84 @@
 import praw
 import config
 import logging
-import time
 import os
+from sklearn.ensemble import RandomForestClassifier
 from metrics import metrics
+import pickle
+
+# TODO: store comments that were replied to in SQLITE db instead of text file
+
+class bot:
+
+    def __init__(self):
+        pass
 
 
-def botLogin():
-    try:
-        r = praw.Reddit(username = config.username,
-                        password = config.password,
-                        client_id = config.client_id,
-                        client_secret = config.client_secret,
-                        user_agent = "some_bot_checker test bot v0.1")
-    except:
-        logging.error('Login Failed')
+    def botLogin(self):
+        try:
+            r = praw.Reddit(username = config.username,
+                            password = config.password,
+                            client_id = config.client_id,
+                            client_secret = config.client_secret,
+                            user_agent = "some_bot_checker test bot v1.0")
+        except:
+            logging.error('Login Failed')
 
-    return r
+        return r
 
+    #Outputs probability that a user is a bot
+    def __makePrediction(self, data):
+        abspath = os.path.abspath(__file__)
+        dname = os.path.dirname(abspath)
+        os.chdir(dname)
 
-def runBot(r, commentsRepliedTo):
+        try:
+            with open("RF_model_for_bot.pkl", 'rb') as file:
+                print(data)
+                model = pickle.load(file)
+                print("loaded")
+                return model.predict_proba(data)
+        except:
+            logging.error('No .pkl file found')
 
-    commentsRepliedTo = []
+    #
+    def runBot(self, r):
+        logging.info('Running...')
+        commentsRepliedTo = []
 
-    for comment in r.subreddit('test').comments(limit=25):
-        if "!isBot" in comment.body and comment.id not in commentsRepliedTo:
+        for mention in r.inbox.mentions(limit = 25):
+            print('stuff')
             logging.info('Comment found')
-            logging.info('Comment.id is: ' + comment.id)
+            logging.info('Comment.id is: ' + mention.id)
 
             met = metrics()
-            authHist = met.getCommentHistory(r, comment.parent().author)
-            authText = met.getAuthorText(authHist)
-            comment.reply("similarity score: " + str(met.getAvgCosineSimilarity(authText)))
-            #comment.reply("avg reply time: " + str(met.getAvgReplyTime(authHist['body'].tolist()) + "seconds"))
 
-            commentsRepliedTo.append(comment.id)
+            parentAuth = mention.parent().author
+            authData = met.getAuthorData(r, parentAuth)
+
+            botProbability = str(self.__makePrediction(met.aggregateMetrics(authData))[:, 1])
+            botProbability = botProbability[1:-5]
+
+            mention.reply("User " + str(parentAuth) +
+                          " is a bot with a probability of " +
+                          str(botProbability) + "%")
+
+            commentsRepliedTo.append(mention.id)
             with open("commentsRepliedTo.txt", "a") as f:
-                f.write(comment.id + "\n")
+                f.write(mention.id + "\n")
 
-    time.sleep(20)
 
 def getSavedComments():
-    #windows os makes me :(
-    os.chdir('c:\\Users\\matt\\Desktop')
-
-    if not os.path.isfile('commentsRepliedTo.txt'):
+    if not os.path.isfile("commentsRepliedTo.txt"):
         commentsRepliedTo = []
-        return commentsRepliedTo
-
     with open("commentsRepliedTo.txt", "r") as f:
         commentsRepliedTo = f.read()
         commentsRepliedTo = commentsRepliedTo.split("\n")
         commentsRepliedTo = filter(None, commentsRepliedTo)
         return commentsRepliedTo
 
-
 commentsRepliedTo = getSavedComments()
-r = botLogin()
-runBot(r, commentsRepliedTo)
+
+redditBot = bot()
+r = redditBot.botLogin()
+redditBot.runBot(r)
